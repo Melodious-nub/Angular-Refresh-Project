ed05e8d58103c0f289fffd39a9be6ca132a64ebb
Melodious-nub <shawon.taluckder1@gmail.com>
2024-11-20 13:42:28 +0600

    updated with signals basics

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,15 +11,27 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // default way zone.js and angular default change detection machanism
+  // selectedUser = DUMMY_USERS[randomIndex];
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+  //   this.selectedUser = DUMMY_USERS[randomIndex];
+  // }
+
+  // using signal
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
+    // for updating signal data
+    this.selectedUser.set(DUMMY_USERS[randomIndex]) ;
+  }  
 }
