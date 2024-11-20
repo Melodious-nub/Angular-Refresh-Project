c74c97ea2c17d4de1fe21545e229c5872ec4e8a8
Melodious-nub <shawon.taluckder1@gmail.com>
2024-11-20 23:02:33 +0600

    updated with task list filter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,13 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, output, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+interface User {
+  id: string,
+  avatar: string,
+  name: string
+}
 
 @Component({
   selector: 'app-user',
@@ -25,13 +31,45 @@ export class UserComponent {
   // }
 
   // using signal
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+  //   // for updating signal data
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]) ;
+  // }
+
+
+  // input in default way
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  // user as object
+  // @Input({required: true}) user!: {
+  //   id: string,
+  //   avatar: string,
+  //   name: string
+  // }
+
+  // user input with ref. interface
+  @Input({required: true}) user!: User;
+  // output common way used from begining
+  @Output() selectUser = new EventEmitter<string>();
+  // output the mordern way (does actuly same as decorator)
+  // selectUser = output<string>();
+
+  get imagePath() {
+      return 'assets/users/' + this.user.avatar;
+  }
+
+  // signal ways input
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => {return 'assets/users/' + this.avatar()})  
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
-    // for updating signal data
-    this.selectedUser.set(DUMMY_USERS[randomIndex]) ;
-  }  
+    this.selectUser.emit(this.user.id)
+  }
 }
